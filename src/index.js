4cb114fb229c2101548577a7c26f5b6f4b66377c
Akylles Barros <42787020+Akyllesbarros@users.noreply.github.com>
2021-09-15 21:48:21 -0300

    att

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@
 const rotas = require('./routes')
 const express = require('express') 
 const app = express()
+var cors = require('cors')
+
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 
 app.use(express.json())
 app.use(rotas)
